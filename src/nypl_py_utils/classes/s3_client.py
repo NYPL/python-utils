cd3a6e26d45214dc36d8638c5376a587d584f438
fatimarahman <fatimarahman@nypl.org>
2026-07-21 13:58:11 -0400

    Allow user to specify encoding

--- src/nypl_py_utils/classes/s3_client.py
+++ src/nypl_py_utils/classes/s3_client.py
@@ -16,13 +16,14 @@ class S3Client:
     """
 
     def __init__(self, bucket, resource=None):
-        self.logger = create_log('s3_client')
+        self.logger = create_log("s3_client")
         self.bucket = bucket
         self.resource = resource
 
         try:
             self.s3_client = boto3.client(
-                's3', region_name=os.environ.get('AWS_REGION', 'us-east-1'))
+                "s3", region_name=os.environ.get("AWS_REGION", "us-east-1")
+            )
         except ClientError as e:
             error_msg = f'Could not create S3 client: {e}'
             self.logger.error(error_msg)
@@ -34,7 +35,7 @@ class S3Client:
     def fetch_cache(self):
         """Fetches a JSON file from S3 and returns the resulting dictionary"""
         self.logger.info(
-            f'Fetching {self.resource} from S3 bucket {self.bucket}')
+            f"Fetching {self.resource} from S3 bucket {self.bucket}")
         try:
             output_stream = BytesIO()
             self.s3_client.download_fileobj(
@@ -42,27 +43,29 @@ class S3Client:
             return json.loads(output_stream.getvalue())
         except ClientError as e:
             error_msg = (
-                f'Error retrieving {self.resource} from S3 bucket '
-                f'{self.bucket}: {e}')
+                f"Error retrieving {self.resource} from S3 bucket "
+                f"{self.bucket}: {e}"
+            )
             self.logger.error(error_msg)
             raise S3ClientError(error_msg) from None
 
     def set_cache(self, state):
         """Writes a dictionary to JSON and uploads the resulting file to S3"""
         self.logger.info(
-            f'Setting {self.resource} in S3 bucket {self.bucket} to {state}')
+            f"Setting {self.resource} in S3 bucket {self.bucket} to {state}"
+        )
         try:
             input_stream = BytesIO(json.dumps(state).encode())
             self.s3_client.upload_fileobj(
                 input_stream, self.bucket, self.resource)
         except ClientError as e:
             error_msg = (
-                f'Error uploading {self.resource} to S3 bucket '
-                f'{self.bucket}: {e}')
+                f"Error uploading {self.resource} to S3 bucket "
+                f"{self.bucket}: {e}")
             self.logger.error(error_msg)
             raise S3ClientError(error_msg) from None
 
-    def upload_file(self, content, file_path):
+    def upload_file(self, content, file_path, encode=True):
         """
         Writes an arbitrary file to S3. Note that this will overwrite any
         existing file with the same name.
@@ -74,16 +77,22 @@ class S3Client:
         file_path: str
             The full path of the file that should be written not including the
             bucket. Example: "subdirectory/example_file.csv"
+        encode: bool, optional
+            Whether to encode the content as utf-8 before writing. Default is
+            True. If False, the content should be provided in bytes (e.g.
+            in parquet, PDF, or image form)
         """
-        self.logger.info(
-            f'Writing {file_path} in S3 bucket {self.bucket}')
+        self.logger.info(f"Writing {file_path} in S3 bucket {self.bucket}")
         try:
-            input_stream = BytesIO(content.encode())
+            if encode:
+                content = content.encode("utf-8")
+            input_stream = BytesIO(content)
             self.s3_client.upload_fileobj(input_stream, self.bucket, file_path)
         except ClientError as e:
             error_msg = (
-                f'Error uploading {file_path} to S3 bucket '
-                f'{self.bucket}: {e}')
+                f"Error uploading {file_path} to S3 bucket "
+                f"{self.bucket}: {e}"
+            )
             self.logger.error(error_msg)
             raise S3ClientError(error_msg) from None
 
