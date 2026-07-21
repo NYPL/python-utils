36266fb1fea6690c78e099931a37f26d0752366f
fatimarahman <fatimarahman@nypl.org>
2026-07-21 14:07:36 -0400

    Add double quotes and update project version

--- tests/test_s3_client.py
+++ tests/test_s3_client.py
@@ -3,20 +3,20 @@ import pytest
 
 from nypl_py_utils.classes.s3_client import S3Client
 
-_TEST_STATE = {'key1': 'val1', 'key2': 'val2', 'key3': 'val3'}
+_TEST_STATE = {"key1": "val1", "key2": "val2", "key3": "val3"}
 
 
 class TestS3Client:
 
     @pytest.fixture
     def test_instance(self, mocker):
-        mocker.patch('boto3.client')
-        return S3Client('test_s3_bucket', 'test_s3_resource')
+        mocker.patch("boto3.client")
+        return S3Client("test_s3_bucket", "test_s3_resource")
 
     def test_fetch_cache(self, test_instance):
         def mock_download(bucket, resource, stream):
-            assert bucket == 'test_s3_bucket'
-            assert resource == 'test_s3_resource'
+            assert bucket == "test_s3_bucket"
+            assert resource == "test_s3_resource"
             stream.write(json.dumps(_TEST_STATE).encode())
 
         test_instance.s3_client.download_fileobj.side_effect = mock_download
@@ -26,12 +26,12 @@ class TestS3Client:
         test_instance.set_cache(_TEST_STATE)
         arguments = test_instance.s3_client.upload_fileobj.call_args.args
         assert arguments[0].getvalue() == json.dumps(_TEST_STATE).encode()
-        assert arguments[1] == 'test_s3_bucket'
-        assert arguments[2] == 'test_s3_resource'
+        assert arguments[1] == "test_s3_bucket"
+        assert arguments[2] == "test_s3_resource"
 
     def test_upload_file(self, test_instance):
-        test_instance.upload_file('test_content', 'test_filename.txt')
+        test_instance.upload_file("test_content", "test_filename.txt")
         arguments = test_instance.s3_client.upload_fileobj.call_args.args
-        assert arguments[0].getvalue() == b'test_content'
-        assert arguments[1] == 'test_s3_bucket'
-        assert arguments[2] == 'test_filename.txt'
+        assert arguments[0].getvalue() == b"test_content"
+        assert arguments[1] == "test_s3_bucket"
+        assert arguments[2] == "test_filename.txt"
